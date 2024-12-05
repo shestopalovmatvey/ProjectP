9357ee1707fbb37fc794506de13d7e29cb023e00
Matvey <m.shestopalov@automacon.ru>
2024-12-06 00:14:20 +0700

    Front: добавил chakraui

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "@assets/styles/main.scss";
 import { App } from "./app";
+import { Provider } from "@/components/ui/provider";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Provider>
+    <App />
+  </Provider>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tsconfigPaths()],
   server: {
     watch: {
       usePolling: true,

7aa8a86e9f976d2194aa807a8a7ce8120013a3d4
Matvey <m.shestopalov@automacon.ru>
2024-12-13 23:21:56 +0700

    Front: test

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -2,8 +2,12 @@ import { Tabbar } from "@/widgets/Tabbar/ui/Tabbar";
 import { AppRouter } from "../providers/RouteProvider";
 import { BrowserRouter } from "react-router";
 import { ModalManager } from "@/widgets/Modals";
+import { useEffect } from "react";
 
 export const App = () => {
+  useEffect(() => {
+    window.Telegram.WebApp.expand();
+  });
   return (
     <>
       <BrowserRouter>
